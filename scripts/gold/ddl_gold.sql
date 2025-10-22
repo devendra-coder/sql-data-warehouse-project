71147fbbb9adc8ed4a1781c1770e69a37405af79
devendra-coder <devendras162002@gmial.com>
2025-10-22 12:26:17 +0530

    ddl for the gold layer completed and added

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -43,22 +43,61 @@ on ci.cst_key = la.cid
 GO
 
 
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+
+if OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products AS
 SELECT
-pn.prd_id,
-pn.cat_id,
-pn.prd_key,
-pn.prd_nm,
-pn.prd_cost,
-pn.prd_line,
-pn.prd_start_dt,
-pn.prd_end_dt,
-pc.cat,
-pc.subcat,
-pc.maintenance
+    ROW_NUMBER() over(order by pn.prd_start_dt, pn.prd_key) as product_key,
+    pn.prd_id as product_id,
+    pn.prd_key as product_number,
+    pn.prd_nm as product_name,
+    pn.cat_id as category_id,
+    pc.cat as category,
+    pc.subcat as subcategory,
+    pc.maintenance as maintenance,
+    pn.prd_cost as cost,
+    pn.prd_line as product_line,
+    pn.prd_start_dt as start_date
 from silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
 on pn.prd_key = pc.id
 WHERE prd_end_dt is NULL   -- filter out all historical data
+GO
+
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customer cu
+    ON sd.sls_cust_id = cu.customer_id;
+GO
+
+
+
  
 
 
